fb74e5f77fad7f017688f232ce2bdd2cbb0522b9
Jack Schaible <jschaible@laika.com>
2022-11-29 12:53:16 -0700

    Convert to node app

--- readme.md
+++ readme.md
@@ -3,22 +3,18 @@
 :heart::game_die: An  HP dice roller for D&D 5e
 
 ## Prerequisites
-- :factory: .NET Core 3.1 SDK
-- :computer: .NET CLI
+- :factory: .NodeJS v16
 
 ## Installation
 1. :arrow_down_small: Download the repo
-1. `dotnet publish`
-1. Add `${PROFILE}\bin\HP_Roller` to your PATH
+1. `npm i && npm run start`
 1. Try it out!
-    - `hrp -h` for help
-    - `hpr {#} {#} {#}d{#}+{#}`, for example `hpr 2 4 3d4+3`, gives you:
         ```
-        Monster # | Hp Rolled | Initiative
+        (index)   | HP Rolled | Initiative
         ----------------------------------
         ----------------------------------
-            1     |   14      |    18
+            0     |   14      |    18
         ----------------------------------
-            2     |   14      |    12
+            1     |   14      |    12
         ----------------------------------
         ```
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import inquirer, { Question } from "inquirer";
+
+const HitDiceRegex = /([0-9]+)d([0-9]+)((?:\+|-)([0-9]+))?/;
+const questions: Question[] = [
+  {
+    type: "number",
+    name: "numMonsters",
+    message: "How many monsters are you rolling for?",
+    default: 1,
+    validate: (input: number) => {
+      if (input > 0) {
+        return true;
+      }
+      return "You must roll for at least one monster";
+    },
+  },
+  {
+    type: "input",
+    name: "hitDice",
+    message: "What is the hit dice of the monster?",
+    default: "1d8",
+    validate: (input: string) => {
+      if (HitDiceRegex.test(input)) {
+        return true;
+      }
+      return "Invalid hit dice";
+    },
+  },
+  {
+    type: "number",
+    name: "initiative",
+    message: "What is the initiative bonus of the monster?",
+    default: 0,
+  },
+];
+let quit = false;
+async function main() {
+  while (!quit) {
+    await run();
+
+    const answers = await inquirer.prompt({
+      type: "confirm",
+      name: "again",
+      message: "Roll again?",
+      default: true,
+    });
+
+    quit = !answers.again;
+  }
+}
+
+async function run() {
+  const answers = await inquirer.prompt(questions);
+  const numMonsters = answers["numMonsters"] as number;
+  const hitDice = answers["hitDice"] as string;
+  const initMod = answers["initiative"] as number;
+
+  const match = hitDice.match(HitDiceRegex);
+
+  if (match) {
+    const numDice = parseInt(match[1] ?? "0");
+    const diceSize = parseInt(match[2] ?? "0");
+    const modifier = match[3] ? parseInt(match[3]) : 0;
+
+    const results: result[] = [];
+    for (let i = 0; i < numMonsters; i++) {
+      let hp = 0;
+      for (let j = 0; j < numDice; j++) {
+        let roll = 0;
+        while (roll < 1) {
+          roll = Math.floor(Math.random() * diceSize) + 1;
+        }
+        hp += roll + modifier;
+      }
+
+      const initiative = Math.floor(Math.random() * 20) + 1 + initMod;
+
+      results.push({
+        "HP Rolled": hp,
+        Initiative: initiative,
+      });
+    }
+
+    results.sort((a, b) => b.Initiative - a.Initiative);
+
+    console.table(results, ["HP Rolled", "Initiative"]);
+  }
+}
+
+interface result {
+  "HP Rolled": number;
+  Initiative: number;
+}
+
+main();
